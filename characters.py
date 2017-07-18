de21c7714bee3131f5edf6156690d995659b5752
Adjectival <alexd.jacks@gmail.com>
2017-07-17 20:30:34 -0700

    another loop

---
+++ characters.py
@@ -0,0 +1,7 @@
+characters = ["Mario", "Peach", "Bowser", "Link"]
+name_lengths = []
+for name in characters:
+    length = len(name)
+    name_lengths.append(length)
+    print name
+print(name_lengths)
