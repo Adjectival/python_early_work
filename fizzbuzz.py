fd968562187b25561d0c7455d1cbd05d47bada9b
Adjectival <alexd.jacks@gmail.com>
2017-07-24 19:23:16 -0700

    fizzbuzz accepts input for upper range bound; input checker built for custom feedback after user input

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+def is_divisible(number, divisor):
+    	return number % divisor == 0
+
+def fizzbuzz_logic(num):
+	if is_divisible(num, 15):
+		return 'FizzBuzz!'
+	elif is_divisible(num, 5):
+		return 'Buzz'
+	elif is_divisible(num, 3):
+		return 'Fizz'
+	else:
+		return num
+
+def fizzbuzz():
+	for i in range(1, int((input('How far to fizz?: ')))):
+		output = fizzbuzz_logic(i)
+		print(output)
+
+fizzbuzz()
--- hackman.py
+++ hackman.py
@@ -4,28 +4,6 @@
 # 3. Check whether the guess was correct or not
 # 4. Display the target word with guessed letters in their correct position, and unguessed letters as underscores
 # 5. Display all their incorrect guesses, as well as their remaining guesses
-
-
-def is_divisible(number, divisor):
-	return number % divisor == 0
-
-def fizzbuzz_logic(num):
-	if is_divisible(num, 15):
-		return 'FizzBuzz!'
-	elif is_divisible(num, 5):
-		return 'Buzz'
-	elif is_divisible(num, 3):
-		return 'Fizz'
-	else:
-		return num
-
-def fizzbuzz(i):
-	i = int(input('How far to fizz?'))
-	for j in range(1, i):
-		output = fizzbuzz(j)
-		print(output)
-
-fizzbuzz()
  
 # 2nd attempt
 # hackman_word = 'hellos'
